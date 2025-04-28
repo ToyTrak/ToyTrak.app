2d3bbcbd203021c85bfa1bbf0f6e6205af141e79
Kenny | 肯尼 <darleenfairy33@myyahoo.com>
2025-04-28 18:51:14 -0400

    1.0-beta Upload #00: Main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+# WINDOW & WINDOW INFORMATION.
+window = Tk()
+window.title("ToyTrak")
+lbl = Label(window, text="Hello, User!")
+lbl.grid(column=0, row=0)
+window.mainloop()
